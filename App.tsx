ac0e47c1c5ba3b28e70969af7dc6b4f611b4df79
Liam Percy <liampercy123@gmail.com>
2021-05-11 21:18:06 +1000

    feat: initial commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { View, Text } from 'react-native';
+
+const App: FC = () => (
+    <View>
+        <Text>App</Text>
+    </View>
+);
+
+export default App;
